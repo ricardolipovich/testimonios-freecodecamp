29b2e53d4c02c92131f2d354db312610d33e1bf7
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-08-04 11:30:46 -0300

    Reto nombre alt dinamico

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -4,7 +4,7 @@ import '../hojas-de-estilo/Testimonio.css'
 function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
-      <img className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt="Foto de Emma" />
+      <img className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt={(`Foto de ${props.nombre}`)} />
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio"><strong>{props.nombre}</strong> in {props.pais}</p>
         <p className="cargo-testimonio">{props.cargo} at <strong>{props.empresa}</strong></p>
