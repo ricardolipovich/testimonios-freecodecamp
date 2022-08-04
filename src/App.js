e08346e7a0de6850129f2b2e86ea0c00ee1a8173
Ricardo Lipovich <ricardolipovich@gmail.com>
2022-08-04 11:18:44 -0300

    Reto Negritas

--- src/App.js
+++ src/App.js
@@ -13,21 +13,26 @@ function App() {
       imagen="shawn"
       cargo="Software Engineer"
       empresa="Amazon"
-      testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life. " />
+      testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. "
+      negrita3="freeCodeCamp changed my life."/>
       <Testimonio 
       nombre="Sarah Chima"
       pais="Nigeria"
       imagen="sarah"
       cargo="Software Engineer"
       empresa="ChatDesk"
-      testimonio="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company. " />
+      testimonio=" The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." 
+      negrita="freeCodeCamp was the gateway to my career as a software developer." />
+      
       <Testimonio 
       nombre="Emma Bostian"
       pais="Sweden"
       imagen="emma"
       cargo="Software Engineer"
       empresa="Spotify"
-      testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify. " />
+      testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on  " 
+      negrita2="freeCodeCamp gave me the skills"
+      testimonio2="and confidence I needed to land my dream job as a software engineer at Spotify."/>
       
       
       </div>
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -8,7 +8,7 @@ function Testimonio(props) {
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio"><strong>{props.nombre}</strong> in {props.pais}</p>
         <p className="cargo-testimonio">{props.cargo} at <strong>{props.empresa}</strong></p>
-        <p className="texto-testimonio">"{props.testimonio}"</p>
+        <p className="texto-testimonio">" <strong>{props.negrita}</strong> {props.testimonio} <strong>{props.negrita2}</strong> {props.testimonio2} <strong>{props.negrita3}</strong>"</p>
 
       </div>
     </div>
